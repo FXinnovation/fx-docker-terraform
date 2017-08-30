3b0bae43326c2790c41b40364d13aaf05db2e79a
Pierre Fortin Carrier <pierre.fortincarrier@gmail.com>
2017-08-30 13:14:03 -0400

    use $TF_PLUGINS_DIR instead of hardcoded path to plugins dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 COMMAND=$1
 
 if [[ "${COMMAND}" == "init" ]] ; then
-  TERRAFORM_OPT="-get-plugins=false -plugin-dir /usr/lib/terraform-provider"
+  TERRAFORM_OPT="-get-plugins=false -plugin-dir ${TF_PLUGINS_DIR}"
 fi
 
 exec /bin/terraform "$@" ${TERRAFORM_OPT}
